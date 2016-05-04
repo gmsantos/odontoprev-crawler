961571d7e311a43600e9de642e381f5c8e0c43ed
gmsantos <gabriel.ms1@hotmail.com>
2016-05-04 16:13:35 -0300

    trying to loop entries

--- crawler.php
+++ crawler.php
@@ -4,6 +4,11 @@ require_once 'vendor/autoload.php';
 
 use Goutte\Client;
 
+function dd($var)
+{
+    die(var_dump($var));
+}
+
 // Set up search url
 $searchUrl = 'https://www.odontoprev.com.br/redecredenciada/buscaRedeCredenciada?cdMarca=1&produtoAns=320603510&rede=&uf=SP&municipio=355030&especialidade=6&regiao=2&token=724c5345-f15d-4e04-8f65-ba8b12d4950d&pesquisaP=false&bairro=0';
 
@@ -13,8 +18,17 @@ $crawler = $client->request('GET', $searchUrl);
 
 $data = [];
 
-$crawler->filter('hr + table')->each(function ($node) use ($data) {
-    print $node->text()."\n";
+$crawler->filter('hr + table > font[color*="blue"]')->each(function ($dentists) use ($data) {
+    foreach ($dentists as $dentist) {
+        $elements = $dentist->getElementsByTagName('td');
+
+        foreach ($elements as $element) {
+            echo $element->nodeValue . PHP_EOL;
+        }
+
+        dd('fin');
+    }
+
 });
 
 // Output $data to csv file
